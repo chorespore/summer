b14d15984c1d8f7616380742daf285ce5005bd02
Zhai Guangchao <593595845@qq.com>
2020-12-14 22:20:38 +0800

    javadoc

--- src/main/java/com/chao/summer/controller/UserController.java
+++ src/main/java/com/chao/summer/controller/UserController.java
@@ -132,6 +132,26 @@ public class UserController {
     }
 
 
+    /**
+     * Check whether the given {@code CharSequence} contains actual <em>text</em>.
+     * <p>More specifically, this method returns {@code true} if the
+     * {@code CharSequence} is not {@code null}, its length is greater than
+     * 0, and it contains at least one non-whitespace character.
+     * </p>
+     * <pre class="code">
+     * StringUtils.hasText(null) = false
+     * StringUtils.hasText("") = false
+     * StringUtils.hasText(" ") = false
+     * StringUtils.hasText("12345") = true
+     * StringUtils.hasText(" 12345 ") = true
+     * </pre>
+     *
+     * @param ids the {@code CharSequence} to check (may be {@code null})
+     * @return {@code true} if the {@code CharSequence} is not {@code null},
+     * its length is greater than 0, {@link java.lang.String#charAt(int)} and it does not contain whitespace only
+     * @see Character#isWhitespace
+     * @deprecated As of JDK 1.1, replaced by {@link #findByAge(Integer age)}
+     */
     @DeleteMapping("")
     public boolean delete(@RequestBody String[] ids) {
         return userService.removeByIds(Arrays.asList(ids));
