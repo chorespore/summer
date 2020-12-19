70baf1a0d553c010de17dd6d2c295566ac9d66a8
Zhai Guangchao <593595845@qq.com>
2020-12-19 18:21:07 +0800

    streamTest

--- src/main/java/com/chao/summer/entity/User.java
+++ src/main/java/com/chao/summer/entity/User.java
@@ -1,9 +1,13 @@
 package com.chao.summer.entity;
 
 import com.baomidou.mybatisplus.extension.activerecord.Model;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User extends Model<User> {
     private Long id;
     private String name;
--- src/test/java/com/chao/summer/FunctionalTest.java
+++ src/test/java/com/chao/summer/FunctionalTest.java
@@ -1,12 +1,14 @@
 package com.chao.summer;
 
+import com.chao.summer.entity.User;
 import org.junit.jupiter.api.Test;
 
-import java.util.UUID;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class FunctionalTest {
 
@@ -36,6 +38,26 @@ public class FunctionalTest {
         Supplier<String> uuid = () -> UUID.randomUUID().toString().replace("-", "");
 
         System.out.println(uuid.get());
+    }
+
+    @Test
+    public void streamTest() {
+        User user1 = new User(1L, "Alice", 12, "alice@chao.com");
+        User user2 = new User(2L, "Bob", 15, "alice@chao.com");
+        User user3 = new User(3L, "Candy", 17, "alice@chao.com");
+        User user4 = new User(4L, "Dad", 18, "alice@chao.com");
+        User user5 = new User(5L, "Elle", 19, "alice@chao.com");
+        User user6 = new User(6L, "Fox", 20, "alice@chao.com");
+
+        List<User> list = Arrays.asList(user1, user2, user3, user4, user5, user6);
+
+        List<String> res = list.stream().filter((u) -> u.getId() % 2 == 0)
+                .filter((u) -> u.getAge() > 10)
+                .map(u -> u.getName().toUpperCase())
+                .sorted(Comparator.reverseOrder())
+                .limit(2).collect(Collectors.toList());
+
+        System.out.println(res);
     }
 
 
