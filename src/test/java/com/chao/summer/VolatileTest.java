f42cdaf89627a16faf370bae22f53c202fa98c5b
Zhai Guangchao <593595845@qq.com>
2020-12-20 10:34:31 +0800

    Thread.yield()

--- src/test/java/com/chao/summer/VolatileTest.java
+++ src/test/java/com/chao/summer/VolatileTest.java
@@ -1,11 +1,59 @@
 package com.chao.summer;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class VolatileTest {
     private static volatile int num = 0;
+    private static AtomicInteger atomicNum = new AtomicInteger();
+
 
     public static void main(String[] args) throws InterruptedException {
+        System.out.println(Thread.activeCount());
+
+        sum();
+        atomicSum();
+//        pause();
+
+    }
+
+    public static void atomicSum() {
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 1000; j++) {
+                    atomicNum.getAndIncrement();
+                }
+            }).start();
+        }
+
+        while (Thread.activeCount() > 1) {
+            System.out.println("yield");
+            Thread.yield();
+        }
+        System.out.println("atomicNum: " + atomicNum);
+    }
+
+    public static void sum() {
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 1000; j++) {
+                    add();
+                }
+            }).start();
+        }
+
+        while (Thread.activeCount() > 2) {
+            System.out.println("yield");
+            Thread.yield();
+        }
+        System.out.println("num: " + num);
+    }
+
+    public static synchronized void add() {
+        num++;
+    }
+
+    public static void pause() throws InterruptedException {
         new Thread(() -> {
             while (num == 0) {
 
