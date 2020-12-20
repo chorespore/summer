5f49bf921aeca9284b1391047fc4c2a68b04f048
Zhai Guangchao <593595845@qq.com>
2020-12-20 12:32:51 +0800

    AtomicStampedReference

--- src/test/java/com/chao/summer/AtomicTest.java
+++ src/test/java/com/chao/summer/AtomicTest.java
@@ -2,21 +2,41 @@ package com.chao.summer;
 
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicStampedReference;
 
-public class VolatileTest {
+public class AtomicTest {
     private static volatile int num = 0;
     private static AtomicInteger atomicNum = new AtomicInteger();
 
 
     public static void main(String[] args) throws InterruptedException {
-        System.out.println(Thread.activeCount());
+        System.out.println("Thread.activeCount: " + Thread.activeCount());
 
-        sum();
-        atomicSum();
+//        sum();
+//        atomicSum();
+        cas();
 //        pause();
+
 
     }
 
+    public static void cas() {
+        AtomicInteger n = new AtomicInteger(6);
+        n.compareAndSet(16, 18);
+        System.out.println(n);
+        n.compareAndSet(6, 8);
+        System.out.println(n);
+
+
+        // 对于 Integer var = ? 在-128 至 127 之间的赋值， Integer 对象是在 IntegerCache.cache 产生，
+        //会复用已有对象，这个区间内的 Integer 值可以直接使用==进行判断，但是这个区间之外的所有数据，都
+        //会在堆上产生，并不会复用已有对象
+        AtomicStampedReference<Integer> stampedNum = new AtomicStampedReference<>(106, 1);
+        int stamp = stampedNum.getStamp();
+        System.out.println(stampedNum.compareAndSet(106, 18, stamp, stamp + 1));
+        System.out.println(stampedNum.getReference());
+    }
+    
     public static void atomicSum() {
         for (int i = 0; i < 100; i++) {
             new Thread(() -> {
