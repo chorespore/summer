f8711924c8c88fb6e62a555df5136fce8fa7a54f
Chao <zhaiguangchao@cmict.chinamobile.com>
2020-12-22 09:40:13 +0800

    StaticProxyTest

---
+++ src/test/java/com/chao/summer/StaticProxyTest.java
@@ -0,0 +1,49 @@
+package com.chao.summer;
+
+public class StaticProxyTest {
+
+    public static void main(String[] args) {
+        new WeddingCompany(new You("Alice")).marry();
+    }
+}
+
+interface Marry {
+    void marry();
+}
+
+class You implements Marry {
+
+    private String bride;
+
+    public You(String bride) {
+        this.bride = bride;
+    }
+
+    @Override
+    public void marry() {
+        System.out.println("Happy marry to " + bride);
+    }
+}
+
+class WeddingCompany implements Marry {
+    private Marry target;
+
+    WeddingCompany(Marry target) {
+        this.target = target;
+    }
+
+    @Override
+    public void marry() {
+        before();
+        target.marry();
+        after();
+    }
+
+    private void before() {
+        System.out.println("Prepare for Wedding");
+    }
+
+    private void after() {
+        System.out.println("Wedding OK");
+    }
+}
