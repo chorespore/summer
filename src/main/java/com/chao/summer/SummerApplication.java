af9fd4c217576d26cd0dca464bc37726eef18c28
Chao <zhaiguangchao@cmict.chinamobile.com>
2020-12-28 17:01:35 +0800

    Scheduled

--- src/main/java/com/chao/summer/SummerApplication.java
+++ src/main/java/com/chao/summer/SummerApplication.java
@@ -3,8 +3,10 @@ package com.chao.summer;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableScheduling
 @SpringBootApplication
 @MapperScan("com.chao.summer.mapper")
 @EnableRedisHttpSession(redisNamespace = "summer:session", maxInactiveIntervalInSeconds = 864000)
---
+++ src/main/java/com/chao/summer/config/ScheduleConfig.java
@@ -0,0 +1,15 @@
+package com.chao.summer.config;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class ScheduleConfig {
+
+    //    @Scheduled(cron = "0/2 * * * * ?")
+    public void tick() {
+        System.out.println("Scheduled: " + new Date());
+    }
+}
