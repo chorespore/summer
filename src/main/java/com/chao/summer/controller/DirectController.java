106899d741d01700fcef3679f77ca047aca29b3d
Zhai Guangchao <593595845@qq.com>
2021-02-03 22:09:45 +0800

    forward redirect

---
+++ src/main/java/com/chao/summer/controller/DirectController.java
@@ -0,0 +1,29 @@
+package com.chao.summer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+@RequestMapping("location")
+public class DirectController {
+
+    @GetMapping("f")
+    public String forword() {
+        return "forward:https://www.baidu.com";
+    }
+
+    @GetMapping("r")
+    public String redirect1() {
+        return "redirect:https://www.baidu.com";
+    }
+
+    @GetMapping("r2")
+    public void redirect2(HttpServletResponse response) throws IOException {
+        response.sendRedirect("https://www.baidu.com");
+    }
+
+}
