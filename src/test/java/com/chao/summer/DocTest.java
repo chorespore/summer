dadc0da06734be1a87ac0783e29bf57263b12e91
翟广超 <zhaiguangchao@cmict.chinamobile.com>
2022-03-14 10:07:49 +0800

    Add DocTest

---
+++ src/test/java/com/chao/summer/DocTest.java
@@ -0,0 +1,65 @@
+/**
+ * Graphics is the abstract base class for all graphics contexts
+ * -encoding UTF-8 -charset UTF-8
+ * 
+ * @author Sami Shaio
+ * @author Arthur van Hoff
+ * @since 1.0
+ */
+public abstract class DocTest {
+
+    /**
+     * XXXXXXXXXXXXXXXXXXXXXXXXX
+     * <p>
+     * conversion process progresses.
+     * 
+     * @see String
+     * @author Zhuozhe1
+     * @see #method3()
+     * @param x the x-coordinate of the northwest corner
+     * @param y the y-coordinate of the northwest corner
+     * @return <code>true</code> if the image is loaded
+     * @version 1.2
+     * @since 2022-03-14
+     */
+    public abstract boolean method1(int x, int y);
+
+    /**
+     * YYYYYYYYYYYYYYYYYYYYYYY
+     * <p>
+     * Dispose of the system resources used by this graphics context.
+     *
+     * @author Zhuozhe2
+     * @version 1.1
+     * @date 2022-03-14
+     */
+    public void method2() {
+        int a = 1;
+    };
+
+    /**
+     * ZZZZZZZZZZZZZZZZZZZZZZZZZZZ
+     * <p>
+     * Disposes of this graphics context once it is no longer
+     *
+     * @see #method2()
+     * @version 1.1
+     * @author Zhuozhe3
+     * @since 2022-03-14
+     */
+    public void method3() {
+        System.out.println(3);
+    }
+
+    /**
+     * Does Whatever
+     * 
+     * @Author Zhuozhe3
+     * @version 1.2.3
+     * @since 1.2.0
+     *
+     */
+    public void method4() {
+        // .......
+    }
+}
