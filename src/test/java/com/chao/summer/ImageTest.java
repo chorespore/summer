a721d63b05b782861947bf442bf54d42d5551be4
翟广超 <zhaiguangchao@cmict.chinamobile.com>
2022-09-01 13:48:41 +0800

    Add urlFileTest

--- src/test/java/com/chao/summer/ImageTest.java
+++ src/test/java/com/chao/summer/ImageTest.java
@@ -5,9 +5,13 @@ import cn.hutool.core.io.FileUtil;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 import java.nio.file.Files;
 import java.util.Base64;
 
@@ -31,7 +35,35 @@ public class ImageTest {
         System.out.println(base64);
     }
 
-    private byte[] getBytesByURL(String imageUrl) throws IOException {
+    @Test
+    public void urlFileTest() {
+        String url = "https://static.www.tencent.com/uploads/2022/06/02/81715eb2fb84b6b8d5be3c1f9c154a42.png";
+        boolean valid = urlValid(url);
+        byte[] bytes = getBytesByURL(url);
+        System.out.println(valid);
+        System.out.println(bytes != null ? bytes.length : 0);
+        String fileName = url.substring(url.lastIndexOf('/') + 1);
+        String a = fileName.substring(0, fileName.lastIndexOf('.'));
+        System.out.println(fileName);
+        System.out.println(a);
+    }
+
+    private boolean urlValid(String url) {
+        try {
+            URLConnection urlCon = new URL(url).openConnection();
+            //文件存在: ‘HTTP/1.1 200 OK’ 文件不存在: ‘HTTP/1.1 404 Not Found’
+            String message = urlCon.getHeaderField(0);
+            if (message.contains("OK")) {
+                return true;
+            }
+        } catch (IOException e) {
+            return false;
+        }
+        return false;
+    }
+
+
+    private byte[] getBytesByURL(String imageUrl) {
         System.out.println("imageUrl:" + imageUrl);
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         BufferedInputStream bis = null;
@@ -47,6 +79,9 @@ public class ImageTest {
                 baos.write(buf, 0, len);
             }
             return baos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
         } finally {
             try {
                 urlconnection.disconnect();
@@ -56,5 +91,4 @@ public class ImageTest {
             }
         }
     }
-
 }
